1be292eb75288348e0f16b391682f74e6005e5ed
tyokokur <110191996+tyokokur@users.noreply.github.com>
2025-05-27 11:50:22 -0700

    autocommit

--- Census.py
+++ Census.py
@@ -3,6 +3,12 @@ import numpy as np, pandas as pd
 class Census:
     """Analysis of Qualtrics data, designed for UC Berkeley Graduate Assembly 2025 Financial Census
     
+    ...
+    
+    Attributes
+    __________
+    data_df : pandas DataFrame
+        extracted data fr
     """
     def __init__(self, filepath='', header=1, datarange=(17,None), from_file=True, 
                  orig_df=pd.DataFrame({'empty':[0]}), orig_datarange=(None,None)):
